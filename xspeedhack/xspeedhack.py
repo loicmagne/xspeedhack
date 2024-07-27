37b4d804b6ff9b31cf485958ac0420e08a0cd156
Loïc Magne <loic.magne@outlook.com>
2024-07-27 11:39:52 +0200

    format + fix names

--- xspeedhack/xspeedhack.py
+++ xspeedhack/xspeedhack.py
@@ -1,5 +1,4 @@
 import time
-import time
 import struct
 import platform
 import psutil
@@ -11,19 +10,22 @@ from importlib import resources
 if platform.system() == "Windows":
     import win32file
 
+
 def resolve_resource(resource_name: str) -> Path:
     return Path(resources.files("xspeedhack.bin") / resource_name)
-    
+
+
 RESOURCES = {
     "x86": {
         "dll": resolve_resource("SpeedHackDLL32.dll"),
-        "inj": resolve_resource("xspeedhack32.exe"),
+        "inj": resolve_resource("injector32.exe"),
     },
     "x64": {
         "dll": resolve_resource("SpeedHackDLL64.dll"),
-        "inj": resolve_resource("xspeedhack64.exe"),
+        "inj": resolve_resource("injector64.exe"),
     },
 }
+
 
 def get_pid(process_name: str) -> int:
     for proc in psutil.process_iter():
@@ -31,13 +33,15 @@ def get_pid(process_name: str) -> int:
             return proc.pid
     raise RuntimeError(f"Process {process_name} not open")
 
+
 class SpeedHackClient:
     PIPE_NAME = r"\\.\pipe\SoulsGymSpeedHackPipe"
+
     def __init__(
         self,
         process_name: str = None,
         process_id: int = None,
-        arch: Literal["x86", "x64"] = "x64"
+        arch: Literal["x86", "x64"] = "x64",
     ):
         """
         Args:
@@ -45,7 +49,9 @@ class SpeedHackClient:
             process_id: ID of the process to connect to. If provided, process_name is ignored.
             arch: Architecture of the process to connect to. "x86" for 32-bit processes and "x64" for 64-bit processes.
         """
-        assert process_name is not None or process_id is not None, "Either process_name or process_id must be provided"
+        assert (
+            process_name is not None or process_id is not None
+        ), "Either process_name or process_id must be provided"
 
         if process_name is not None:
             self.pid = get_pid(process_name)
@@ -56,7 +62,9 @@ class SpeedHackClient:
         self.arch = arch
 
         self.inject_dll(self.pid)
-        time.sleep(1)  # Give the pipe time to come up after the injection (very conservative)
+        time.sleep(
+            1
+        )  # Give the pipe time to come up after the injection (very conservative)
         self.pipe = self._connect_pipe()
 
     def set_speed(self, value: float):
@@ -64,17 +72,26 @@ class SpeedHackClient:
         win32file.WriteFile(self.pipe, struct.pack("f", value))
 
     def _connect_pipe(self) -> int:
-        return win32file.CreateFile(self.PIPE_NAME, win32file.GENERIC_WRITE, 0, None,
-                                    win32file.OPEN_EXISTING, 0, None)
+        return win32file.CreateFile(
+            self.PIPE_NAME,
+            win32file.GENERIC_WRITE,
+            0,
+            None,
+            win32file.OPEN_EXISTING,
+            0,
+            None,
+        )
 
     def __del__(self):
         """Close the pipe handle on deletion."""
         if self.pipe is not None:
             try:
                 win32file.CloseHandle(self.pipe)
-            except TypeError:  # Throws NoneType object not callable error for some reason
+            except (
+                TypeError
+            ):  # Throws NoneType object not callable error for some reason
                 ...
-    
+
     def inject_dll(self, pid: int):
         path_dll = RESOURCES[self.arch]["dll"]
         path_injector = RESOURCES[self.arch]["inj"]
